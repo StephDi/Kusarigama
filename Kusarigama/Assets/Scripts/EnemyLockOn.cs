b6b217a2d12ca80d114888177c54a942c9006f5b
Schroeder <Florio97@web.de>
2019-06-18 13:43:02 +0200

    EnemyLockOn CamChange

--- Kusarigama/Assets/Scripts/EnemyLockOn.cs
+++ Kusarigama/Assets/Scripts/EnemyLockOn.cs
@@ -5,6 +5,21 @@ using UnityEngine;
 public class EnemyLockOn : MonoBehaviour
 {
 
-    
+    public GameObject playerCamera;
+    public GameObject lockOnCamera;
+
+
+    void Update()
+    {
+        if(Input.GetButtonDown("LockOn") && playerCamera.activeSelf == true)
+        {
+            lockOnCamera.SetActive(true);
+            playerCamera.SetActive(false);
+        }else if(Input.GetButtonDown("LockOn") && lockOnCamera.activeSelf == true)
+        {
+            playerCamera.SetActive(true);
+            lockOnCamera.SetActive(false);
+        }
+    }
 
 }
