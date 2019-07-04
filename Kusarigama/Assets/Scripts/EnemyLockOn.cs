0e7a9608b9d5b174ceab72000d81524b9b37bf33
Schroeder <Florio97@web.de>
2019-07-05 01:19:13 +0200

    better LockOnCam step 1

--- Kusarigama/Assets/Scripts/EnemyLockOn.cs
+++ Kusarigama/Assets/Scripts/EnemyLockOn.cs
@@ -1,25 +1,77 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine.Editor;
 
 public class EnemyLockOn : MonoBehaviour
 {
-
+    public Transform player;
     public GameObject playerCamera;
     public GameObject lockOnCamera;
+    public GameObject[] enemies;
+    public GameObject closestEnemy;
+    public Cinemachine.CinemachineTargetGroup group;
+    public Cinemachine.CinemachineTargetGroup.Target[] targets;
 
+    private void Start()
+    {
+        targets = group.m_Targets;
+    }
 
     void Update()
     {
-        if(Input.GetButtonDown("LockOn") && playerCamera.activeSelf == true)
+
+        if (Input.GetButtonDown("LockOn") && playerCamera.activeSelf == true)
         {
-            lockOnCamera.SetActive(true);
-            playerCamera.SetActive(false);
-        }else if(Input.GetButtonDown("LockOn") && lockOnCamera.activeSelf == true)
-        {
-            playerCamera.SetActive(true);
-            lockOnCamera.SetActive(false);
+            ActivateLockOncam();
+            FindClosestEnemy();
         }
+        else if (Input.GetButtonDown("LockOn") && lockOnCamera.activeSelf == true)
+        {
+            ActivatePlayerCam();
+        }
+
     }
 
+    void ActivateLockOncam()
+    {
+        lockOnCamera.SetActive(true);
+        playerCamera.SetActive(false);
+    }
+
+    void ActivatePlayerCam()
+    {
+        playerCamera.SetActive(true);
+        lockOnCamera.SetActive(false);
+    }
+
+    void FindClosestEnemy()
+    {
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        var distance = Mathf.Infinity;
+        var position = player.transform.position;
+        foreach (GameObject e in enemies)
+        {
+            var diff = (e.transform.position - position);
+            var curDistance = diff.sqrMagnitude;
+
+            if (curDistance < distance)
+            {
+                closestEnemy = e;
+                distance = curDistance;
+                if (curDistance < 1000f)
+                {                  
+                    targets[0] = new Cinemachine.CinemachineTargetGroup.Target { target = player, radius = 4.0f, weight = 1.0f };
+                    targets[1] = new Cinemachine.CinemachineTargetGroup.Target { target = closestEnemy.transform, radius = 4.0f, weight = 1.0f };                 
+                }
+                else if (curDistance > 1000f)
+                {
+                    targets[0] = new Cinemachine.CinemachineTargetGroup.Target { target = player, radius = 4.0f, weight = 1.0f };
+                    targets[1] = new Cinemachine.CinemachineTargetGroup.Target { target = null, radius = 4.0f, weight = 1.0f };
+                    ActivatePlayerCam();
+                }
+
+            }
+        }
+    }
 }
