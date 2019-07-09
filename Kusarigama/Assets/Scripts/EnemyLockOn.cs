dde0273c6941cd7f1bcd50e642f9fe3dda71d48c
Schroeder <Florio97@web.de>
2019-07-09 02:13:42 +0200

    LockOnCam step 3

--- Kusarigama/Assets/Scripts/EnemyLockOn.cs
+++ Kusarigama/Assets/Scripts/EnemyLockOn.cs
@@ -6,33 +6,44 @@ using Cinemachine.Editor;
 public class EnemyLockOn : MonoBehaviour
 {
     public Transform player;
+    //Cameras
     public GameObject playerCamera;
     public GameObject lockOnCamera;
     public GameObject mainCamera;
+    //Enemies
     public GameObject[] enemies;
-    public GameObject closestEnemy;
+    public GameObject closestEnemy;  
+    //Raycast
+    public bool enemyObstructed;
+    //Target
     public Cinemachine.CinemachineTargetGroup group;
     public Cinemachine.CinemachineTargetGroup.Target[] targets;
-    public bool enemyObstructed;
+    public Transform targetIndicator;
+    //Timer
+    public float waitForUnLock;
+    public bool waited;
 
     private void Start()
     {
         targets = group.m_Targets;
+        waitForUnLock = 0.3f;
     }
 
     void Update()
     {
-
+        //Press the Button to Lock on 
         if (Input.GetButtonDown("LockOn") && playerCamera.activeSelf == true && enemyObstructed == false)
         {
             ActivateLockOncam();
             FindClosestEnemy();
+            targetIndicator.gameObject.SetActive(true);
         }
-        else if (Input.GetButtonDown("LockOn") && lockOnCamera.activeSelf == true)
+        else if (Input.GetButtonDown("LockOn") && lockOnCamera.activeSelf == true || closestEnemy == null || enemyObstructed == true)
         {
             ActivatePlayerCam();
+            targetIndicator.gameObject.SetActive(false);
         }
-
+       
         //Test if you can see the Enemy
         RaycastHit hit;
         int layerMask = 1 << 8;
@@ -52,9 +63,15 @@ public class EnemyLockOn : MonoBehaviour
                 }
                 else
                 {
-                    enemyObstructed = true;
+                    StartCoroutine(WaitForUnLock());
                 }
             }
+        }
+
+        //TargetIndicator
+        if (closestEnemy != null)
+        {
+            targetIndicator.transform.position = new Vector3(closestEnemy.transform.position.x,closestEnemy.transform.position.y+1,closestEnemy.transform.position.z);
         }
     }
 
@@ -99,5 +116,11 @@ public class EnemyLockOn : MonoBehaviour
     {
         playerCamera.SetActive(true);
         lockOnCamera.SetActive(false);
+    }
+
+    IEnumerator WaitForUnLock()
+    {
+        yield return new WaitForSeconds(waitForUnLock);
+        enemyObstructed = true;
     }
 }
