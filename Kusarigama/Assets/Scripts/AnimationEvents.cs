e1eada736527c417eb5720236c258c4a00651717
Stephi <steffi.dietzel@t-online.de>
2019-08-21 13:42:24 +0200

    neue Animationen Hauptfigur

---
+++ Kusarigama/Assets/Scripts/AnimationEvents.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEvents : MonoBehaviour
+{
+
+    public AudioClip footStep;
+    public AudioSource audioS;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Footstep()
+    {
+        audioS.PlayOneShot(footStep);
+    }
+}
