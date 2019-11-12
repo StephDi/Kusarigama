8eaebee2f75b8d58e2628a0176d4985914e5ee4a
Schroeder <Florio97@web.de>
2019-11-12 01:45:16 +0100

    RangedCombat3 + MenuScreen

---
+++ Kusarigama/Assets/Scripts/UIManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance = null;
+
+    public GameObject panel;
+    public GameObject character;
+    public Cinemachine.CinemachineVirtualCamera menuCam;
+
+    void Awake()
+    {
+        if (instance == null)
+
+            instance = this;
+
+        else if (instance != this)
+
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Exit"))
+        {
+            panel.SetActive(!panel.activeInHierarchy);
+            character.GetComponent<CharMovement>().enabled = !character.GetComponent<CharMovement>().enabled;
+            menuCam.enabled = !menuCam.enabled;
+        }    
+    }
+
+}
