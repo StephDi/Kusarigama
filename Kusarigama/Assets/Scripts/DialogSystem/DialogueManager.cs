b81963ef37e40758583faf83c0ef34f7f205fec3
Schroeder <Florio97@web.de>
2020-05-25 20:59:29 +0200

    DialogSystem, PostProcessingExperiment

---
+++ Kusarigama/Assets/Scripts/DialogSystem/DialogueManager.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    public TMP_Text nameText;
+    public TMP_Text dialogueText;
+
+    public GameObject dialogueWindow;
+
+    private Queue<string> sentences;
+
+    private CharMovement charMovement;
+
+    private void Awake()
+    {
+        charMovement = FindObjectOfType<CharMovement>();
+    }
+    private void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            DisplayNextSentence();
+        }
+    }
+
+    public void StartDialog( Dialogue dialogue)
+    {
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        charMovement.anim.SetBool("moving",false);
+        charMovement.enabled = false;
+        dialogueWindow.SetActive(true);
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    private void EndDialogue()
+    {
+        charMovement.enabled = true;
+        dialogueWindow.SetActive(false);
+        Debug.Log("End");
+    }
+}
---
+++ Kusarigama/Assets/Scripts/DialogSystem/TriggerDialogueOnCollision.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDialogueOnCollision : MonoBehaviour
+{
+    private DialogueTrigger dialogueTrigger;
+
+    private GetWeaponUpgrades getWeaponUpgrades;
+
+    private void Awake()
+    {
+        getWeaponUpgrades = FindObjectOfType<GetWeaponUpgrades>();
+        dialogueTrigger = GetComponent<DialogueTrigger>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player") && getWeaponUpgrades.weaponState == WeaponState.NONE)
+        {
+            dialogueTrigger.TriggerDialogue();
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
